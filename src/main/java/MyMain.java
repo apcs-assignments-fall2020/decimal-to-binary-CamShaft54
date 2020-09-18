d51187e0ce68658be0793d9f00ae6ef08793d9c8
camshaft54 <cam.shaw1234@gmail.com>
2020-09-18 09:55:35 -0700

    Finished

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,13 +2,19 @@ import java.util.Scanner;
 
 public class MyMain {
     
-    public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+    public static int binaryToDecimal(String binary) {
+        String[] binArr = binary.split("");
+        int result = 0;
+        for (int i = 0; i < binArr.length; i++)
+            result += (int) Math.pow(2, binArr.length-1-i) * Integer.parseInt(binArr[i]);
+        return result;
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type a number in binary format:");
+        System.out.println("That is equal to the decimal value: " + binaryToDecimal(scan.next()));
+        scan.close();
     }
 }
